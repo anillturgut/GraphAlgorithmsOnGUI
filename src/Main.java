f5bf64b73bb7af230d464929506f50e72daf4e74
Anıl Turgut <69848213+anillturgut@users.noreply.github.com>
2023-10-27 10:18:52 +0300

    AppIcon is changed.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 import gui.MainWindow;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {  }
 
         JFrame j = new JFrame();
         j.setTitle("Graph & Network GUI");
+        j.setIconImage(ImageIO.read(new File("src/resources/boun.png")));
 
         j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         j.setSize(new Dimension(900, 600));
