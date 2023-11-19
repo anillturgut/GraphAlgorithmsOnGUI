3a08d96269b641842d141ff491248a83915dfaf8
Anıl Turgut <69848213+anillturgut@users.noreply.github.com>
2023-11-19 16:33:26 +0300

    log bug is fixed.

--- src/gui/LogActions.java
+++ src/gui/LogActions.java
@@ -32,6 +32,7 @@ public class LogActions {
     private boolean loggingEnabled;
 
     private static final Logger LOGGER = Logger.getLogger(LogActions.class.getName());
+    private static boolean loggerConfigured = false;
 
 
     public LogActions(Graph graph, GraphPanel graphPanel, boolean loggingEnabled,
@@ -67,57 +68,80 @@ public class LogActions {
         LOGGER.info("Algorithm Completed \n\n");
 
         // Close the logger
-        Logger.getLogger("").removeHandler(Logger.getLogger("").getHandlers()[0]);
+        closeLogger();
     }
     private void setupLogger() {
-        try {
-            // Set the log file location to the user's Documents folder
-            String os = System.getProperty("os.name").toLowerCase();
-            String userHome = System.getProperty("user.home");
-            Path logFolderPath;
+        if(!loggerConfigured){
+            try {
+                // Set the log file location to the user's Documents folder
+                String os = System.getProperty("os.name").toLowerCase();
+                String userHome = System.getProperty("user.home");
+                Path logFolderPath;
 
-            if (os.contains("win")) {
-                logFolderPath = Paths.get(userHome, "Documents", "GraphAlgorithmsOnGUILog");
-            } else if (os.contains("mac")) {
-                logFolderPath = Paths.get(userHome, "Documents", "GraphAlgorithmsOnGUILog");
-            } else {
-                // For other operating systems, you may need to adjust the path accordingly
-                logFolderPath = Paths.get(userHome, "GraphAlgorithmsOnGUILog");
+                if (os.contains("win")) {
+                    logFolderPath = Paths.get(userHome, "Documents", "GraphAlgorithmsOnGUILog");
+                } else if (os.contains("mac")) {
+                    logFolderPath = Paths.get(userHome, "Documents", "GraphAlgorithmsOnGUILog");
+                } else {
+                    // For other operating systems, you may need to adjust the path accordingly
+                    logFolderPath = Paths.get(userHome, "GraphAlgorithmsOnGUILog");
+                }
+
+                // Create log folder if it does not exist
+                if (!Files.exists(logFolderPath)) {
+                    Files.createDirectories(logFolderPath);
+                }
+
+                // Get the current date for the log file name
+                LocalDateTime  currentDate = LocalDateTime.now();
+                // Define the desired date format
+                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+                // Format the date using the specified format
+                String formattedDate = currentDate.format(dateTimeFormatter);
+
+                // Create log file
+                Path logFilePath = logFolderPath.resolve("GraphAlgorithmsLog_" + formattedDate + ".txt");
+
+                // Check if the log file already exists
+                if (!Files.exists(logFilePath)) {
+                    Files.createFile(logFilePath);
+                }
+
+                // Remove existing handlers
+                Logger rootLogger = Logger.getLogger("");
+                for (java.util.logging.Handler handler : rootLogger.getHandlers()) {
+                    rootLogger.removeHandler(handler);
+                }
+
+                // Create a file handler that appends log entries to the log file
+                FileHandler fileHandler = new FileHandler(logFilePath.toString(), true);
+
+                // Create a simple formatter for the log entries
+                SimpleFormatter formatter = new SimpleFormatter();
+                fileHandler.setFormatter(formatter);
+
+                // Set the logging level
+                LOGGER.setLevel(Level.INFO);
+
+                // Add the file handler to the logger if logging is enabled
+                if(loggingEnabled)
+                    LOGGER.addHandler(fileHandler);
+
+                loggerConfigured = true;
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            // Create log folder if it does not exist
-            if (!Files.exists(logFolderPath)) {
-                Files.createDirectories(logFolderPath);
-            }
-
-            // Get the current date for the log file name
-            String currentDate = java.time.LocalDate.now().toString();
-
-            // Create log file
-            Path logFilePath = logFolderPath.resolve("GraphAlgorithmsLog_" + currentDate + ".txt");
-
-            // Check if the log file already exists
-            if (!Files.exists(logFilePath)) {
-                Files.createFile(logFilePath);
-            }
-
-            // Create a file handler that appends log entries to the log file
-            FileHandler fileHandler = new FileHandler(logFilePath.toString(), true);
-
-            // Create a simple formatter for the log entries
-            SimpleFormatter formatter = new SimpleFormatter();
-            fileHandler.setFormatter(formatter);
-
-            // Set the logging level
-            LOGGER.setLevel(Level.INFO);
-
-            // Add the file handler to the logger if logging is enabled
-            if(loggingEnabled)
-                LOGGER.addHandler(fileHandler);
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+    }
+    private static void closeLogger() {
+        // Close the logger
+        Logger rootLogger = Logger.getLogger("");
+        for (java.util.logging.Handler handler : rootLogger.getHandlers()) {
+            rootLogger.removeHandler(handler);
+            handler.close();
         }
     }
 }
